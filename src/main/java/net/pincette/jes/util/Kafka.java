3146a73fcff914f8e7136a4c6b03f5fe141b7364
Werner Donné <werner.donne@pincette.biz>
2020-07-30 21:46:31 +0200

    Add Kafka.fromEnv

--- src/main/java/net/pincette/jes/util/Kafka.java
+++ src/main/java/net/pincette/jes/util/Kafka.java
@@ -1,6 +1,9 @@
 package net.pincette.jes.util;
 
+import static java.lang.System.getenv;
+import static java.util.Arrays.stream;
 import static java.util.Collections.unmodifiableMap;
+import static java.util.stream.Collectors.joining;
 import static java.util.stream.Collectors.toMap;
 import static net.pincette.jes.util.JsonFields.CORR;
 import static net.pincette.util.Collections.map;
@@ -11,8 +14,10 @@ import static org.apache.kafka.streams.kstream.JoinWindows.of;
 import com.typesafe.config.Config;
 import java.time.Duration;
 import java.util.Map;
+import java.util.Map.Entry;
 import java.util.concurrent.CompletableFuture;
 import java.util.concurrent.CompletionStage;
+import java.util.stream.Stream;
 import javax.json.JsonObject;
 import net.pincette.util.Pair;
 import org.apache.kafka.clients.producer.KafkaProducer;
@@ -28,6 +33,7 @@ import org.apache.kafka.streams.kstream.KStream;
  * @since 1.0
  */
 public class Kafka {
+  private static final String KAFKA_PREFIX = "KAFKA_";
   private static final Map<String, Object> RELIABLE_PRODUCER_CONFIG =
       unmodifiableMap(
           map(
@@ -89,6 +95,27 @@ public class Kafka {
   public static Map<String, Object> fromConfig(final Config config, final String path) {
     return config.getConfig(path).entrySet().stream()
         .collect(toMap(Map.Entry::getKey, e -> e.getValue().unwrapped()));
+  }
+
+  /**
+   * Reads environment variables that start with "KAFKA_" and creates a configuration with it. The
+   * underscores become dots and the keywords are set in lower case.
+   *
+   * @return The Kafka configuration.
+   * @since 1.3.3
+   */
+  public static Map<String, Object> fromEnv() {
+    return kafkaEnv().collect(toMap(e -> kafkaProperty(e.getKey()), Entry::getValue));
+  }
+
+  private static Stream<Entry<String, String>> kafkaEnv() {
+    return getenv().entrySet().stream().filter(e -> e.getKey().startsWith(KAFKA_PREFIX));
+  }
+
+  private static String kafkaProperty(final String env) {
+    return stream(env.substring(KAFKA_PREFIX.length()).split("_"))
+        .map(String::toLowerCase)
+        .collect(joining("."));
   }
 
   /**
